09ca2d76e50835154665432383259980178e6122
Beliakov Vladimir <ermegilius@gmail.com>
2024-10-01 12:32:48 +0300

    end of the day 01.10.2024

--- JS/marGIT/6_week/06_exercises/cars/public/script.js
+++ JS/marGIT/6_week/06_exercises/cars/public/script.js
@@ -12,7 +12,7 @@ function validateForm(input) {
 //create Car prototype
 class Car {
     constructor(licensePlate, maker, model, year, currentOwner, price, discountedPrice, color) {
-        this.licensePlate = licensePlate;
+        this.licensePlate = licensePlate.toUpperCase();
         this.maker = maker;
         this.model = model;
         this.year = year;
@@ -23,19 +23,25 @@ class Car {
     }
 }
 
-//this function takes users input and creates a new object with car prototype
+function discountPrice(price) {
+    const DISCOUNT_RATE = 0.85;
+    return (price * DISCOUNT_RATE).toFixed(2);
+}
+
+//this function takes user's input and creates a new object with Car prototype
 function addCar(event) {
     event.preventDefault();//do not drop the table on submit
 
-    const licensePlate = document.querySelector('#licensePlate').value;
-    const maker = document.querySelector('#maker').value;
-    const model = document.querySelector('#model').value;
-    const year = document.querySelector('#year').value;
-    const currentOwner = document.querySelector('#currentOwner').value;
-    const price = +document.querySelector('#price').value;
-    let discountedPrice = "-";//no discount by default
+    const licensePlate = document.querySelector('#licensePlate').value.trim();
+    const maker = document.querySelector('#maker').value.trim();
+    const model = document.querySelector('#model').value.trim();
+    const year = document.querySelector('#year').value.trim();
 
-    const color = document.querySelector('#color').value;
+
+    const currentOwner = document.querySelector('#currentOwner').value.trim();
+    const price = document.querySelector('#price').value.trim();
+    let discountedPrice = "-";//no discount by default
+    const color = document.querySelector('#color').value.trim();
 
     console.log(licensePlate, maker, model, year, currentOwner, price, discountedPrice, color);
 
@@ -47,49 +53,37 @@ function addCar(event) {
         if (!validateForm(licensePlate)) {//license plate can contain only latin letters, figures hyphens and spaces 
             throw new Error("You can only enter either 0-9, A-F, '-' and spaces");
         }
-        if (carsList.some((element) => element.licensePlate === licensePlate)) { //license plate must be unique
+        if (carsList.some((car) => car.licensePlate === licensePlate.toUpperCase())) { //license plate must be unique
             throw new Error("Looks like this car is already in the base (same license plate found)");
         }
-        if (typeof price !== 'number' || price < 0) {//price is positive number
+        if (+price < 0) {//price must be a positive number
             console.log(typeof price);
             throw new Error("Price must be a positive number");
         }
-        if (+year < 1886 && +year !== 0) {//year of production can't be earlier than the first car in history. Empty field doesn't equal to 0.
+        if (+year < 1886 && +year !== 0) {//year of production can't be earlier than the first car in history. Empty field doesn't equal 0.
+            console.log("Emmett,stop with this nonsense");
             throw new Error("The first car was made in 1886. Make sure that you put down the right year");
         }
         if (year > new Date().getFullYear()) {//a car can't be produced in the future
             console.log(typeof year);
-            console.log(`User inserted ${year} year`);
-            throw new Error(`It's ${new Date().getFullYear()} now. Make sure that you put down the right year`);
+            console.log(`
+                User inserted ${year} year. \n
+                Emmett, be a reasonable man...
+                `);
+            throw new Error(`It's ${new Date().getFullYear()} now. Make sure that you put down the right year.\n
+            In a case of some time-space events grab your towel and don't panic!`);
         }
-        if (year !== "" && new Date().getFullYear() - year > 10) { //cars that are older than 10 years should receive a 15% discount on their price
-            const DISCOUNT_RATE = 0.85;
-            discountedPrice = (price * DISCOUNT_RATE).toFixed(2);
+        if (year !== "" && new Date().getFullYear() - +year > 10) { //cars that are older than 10 years should receive a 15% discount on their price
+            discountedPrice = discountPrice(price);
         }
-        const newCar = new Car(licensePlate, maker, model, year, currentOwner, price, discountedPrice, color); //create new object
 
+        const newCar = new Car(licensePlate, maker, model, year, currentOwner, price, discountedPrice, color); //create new object
         carsList.push(newCar); //push the object to carList array
 
-        let table = document.querySelector('#carsTable');//get the table
-        let row = table.insertRow(-1);//add a raw in th bottom of the table
+        //displayTable(newCar);//evoke function whick takes the newCar and inserts it to the table's fields
 
-        //put user's input to the table cell by cell via loop
+        createTable();
 
-        //displaytable separate function!!!!
-        const values = [newCar.licensePlate, newCar.maker, newCar.model, newCar.year, newCar.currentOwner, newCar.price, discountedPrice, newCar.color];
-        values.forEach((value, index) => {
-            let cell = row.insertCell(index);
-            cell.innerText = value;
-            if (index === values.length - 1) { //it's the last cell it a row
-                // Check color luminance to adjust text color. Here I use an external library "tinycolor" to get a value of how bright the collor is. And use it to switch font collor between black and white.
-                cell.style.backgroundColor = newCar.color;// use the cars color to the cell
-                if (tinycolor(newCar.color).getLuminance() < 0.5) {
-                    cell.style.color = '#FFFFFF';//use white font if the color is dark
-                } else {
-                    cell.style.color = '#000';//use black font if the color is light
-                }
-            }
-        });
     } catch (error) {
         alert(`${error}`);
     } finally {
@@ -98,12 +92,55 @@ function addCar(event) {
     searchResult.classList.add('hidden');//hide search result div
 }
 
+// //this function uses created object to add lines to the table. Turned it off to use another one, which creates the table from the cars array.
+// function displayTable(car) {
+//     let table = document.querySelector('#carsTable');//get the table
+//     let row = table.insertRow(-1);//add a raw in th bottom of the table
+//     const values = [car.licensePlate, car.maker, car.model, car.year, car.currentOwner, car.price, car.discountedPrice, car.color];
+//     values.forEach((value, index) => {
+//         let cell = row.insertCell(index);
+//         cell.innerText = value;
+//         if (index === values.length - 1) { //it's the last cell it a row
+//             // Check the color's luminance to adjust text color. Here I use an external library "tinycolor" to get a value of how bright the collor is. And use it to switch font collor between black and white.
+//             cell.style.backgroundColor = car.color;// use the cars color to the cell
+//             if (tinycolor(car.color).getLuminance() < 0.5) {
+//                 cell.style.color = '#FFFFFF';//use white font if the color is dark
+//             } else {
+//                 cell.style.color = '#000';//use black font if the color is light
+//             }
+//         }
+//     });
+// }
+
+//this function uses cars array to create the table
+function createTable() {
+    let table = document.querySelector('#carsTable');//get the table
+    let row = table.insertRow(-1);//add a raw in th bottom of the table
+    carsList.forEach((car) => {
+        const values = [car.licensePlate, car.maker, car.model, car.year, car.currentOwner, car.price, car.discountedPrice, car.color];
+
+        values.forEach((value, index) => {
+            let cell = row.insertCell(index);
+            cell.innerText = value;
+            if (index === values.length - 1) { //it's the last cell it a row
+                // Check the color's luminance to adjust text color. Here I use an external library "tinycolor" to get a value of how bright the collor is. And use it to switch font collor between black and white.
+                cell.style.backgroundColor = car.color;// use the cars color to the cell
+                if (tinycolor(car.color).getLuminance() < 0.5) {
+                    cell.style.color = '#FFFFFF';//use white font if the color is dark
+                } else {
+                    cell.style.color = '#000';//use black font if the color is light
+                }
+            }
+        });
+    });
+}
+
+
 //this function filters through the array and and returns the object keys by license plate match
 function searchCar(event) {
     event.preventDefault();//do not drop the table on submit
     let foundCar = document.querySelector('#foundCar');
-    let licensePlateSearch = document.querySelector('#licensePlateSearch').value;
-    foundCar.innerText = "Let's find a car";
+    let licensePlateSearch = document.querySelector('#licensePlateSearch').value.toUpperCase().trim();
 
     try {
         if (licensePlateSearch === "") {
@@ -112,13 +149,14 @@ function searchCar(event) {
         if (!validateForm(licensePlateSearch)) {
             throw new Error("You can only enter either 0-9, A-F, '-' and spaces");
         }
-        searchResult.classList.remove('hidden');//unhide search result div
+        searchResult.classList.remove('hidden');//unhide search results div
 
         const filtered = carsList.filter((element) => element.licensePlate === licensePlateSearch);
 
         //if the length is more than 0, than apparently if has found something. Than return the keys of this object
         if (filtered.length > 0) {
-            foundCar.innerText = `I found a car: \n
+            foundCar.innerText = `
+                    I found a car: \n
                     licence plate: ${filtered[0].licensePlate}\n
                     maker: ${filtered[0].maker}\n
                     model: ${filtered[0].model}\n
